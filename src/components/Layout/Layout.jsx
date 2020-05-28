cdf780572b63272f11a451b62faa24f4a9900672
Gal94 <yacobi.gal94@gmail.com>
2020-05-29 02:53:34 +0300

    Started working on the Toolbar component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 //props.children is the BurgerBuilder component
 const Layout = (props) => {
   return (
     <React.Fragment>
-      <div>Toolbar, sideDrawer, Backdrop</div>
+      <Toolbar />
       <main className={classes.Content}>{props.children}</main>
     </React.Fragment>
   );
